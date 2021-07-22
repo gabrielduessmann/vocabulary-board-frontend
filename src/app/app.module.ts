8e757f9c9f85d6c37e4e826c01a9459854356781
Duessmann <gabriel.duessmann@gmail.com>
2021-07-21 22:27:45 -0300

    update app component and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VocabularyComponent } from './board/vocabulary/vocabulary.component';
+import { HeaderComponent } from './header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ColumnComponent } from './board/column/column.component';
+import { CardComponent } from './board/column/card/card.component';
+import {BoardComponent} from "./board/board.component";
+import {RoutingModule} from "./routing/routing.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { VocabularyWordComponent } from './board/vocabulary/vocabulary-word/vocabulary-word.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VocabularyComponent,
+    HeaderComponent,
+    ColumnComponent,
+    CardComponent,
+    BoardComponent,
+    VocabularyWordComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
