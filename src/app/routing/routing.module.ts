3780de32a3d17561868f7c919c92f708bdd8da17
Duessmann <gabriel.duessmann@gmail.com>
2021-07-21 22:22:47 -0300

    adding routing service

---
+++ src/app/routing/routing.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {VocabularyComponent} from "../board/vocabulary/vocabulary.component";
+import {BoardComponent} from "../board/board.component";
+
+const routes: Routes = [
+  {path: '', redirectTo: '/board', pathMatch: 'full'},
+  {path: 'board', component: BoardComponent},
+  {path: 'vocabulary', component: VocabularyComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot((routes))],
+  exports: [RouterModule]
+})
+export class RoutingModule {
+
+}
