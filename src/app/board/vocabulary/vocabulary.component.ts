bc4f7ffe425a2093386c736ecab36f82be9c75b8
Duessmann <gabriel.duessmann@gmail.com>
2021-08-01 23:11:05 -0300

    Vocabulary page getting all vocabulary

--- src/app/board/vocabulary/vocabulary.component.ts
+++ src/app/board/vocabulary/vocabulary.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {StatusEnum} from "../column/status.enum";
+import {VocabularyService} from "./vocabulary.service";
+import {Column} from "../column/column.model";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'vocabulary',
@@ -10,13 +13,22 @@ export class VocabularyComponent implements OnInit {
 
   vocabs: string[] = [];
   isEditMode: boolean = false;
-  initialStatus: StatusEnum = StatusEnum.POOL;
 
-  constructor() { }
+  columns: Column[];
+
+  constructor(private vocabularyService: VocabularyService) { }
 
   ngOnInit(): void {
+    this.getAllBoardColumns();
   }
 
+  getAllBoardColumns() {
+    this.vocabularyService.getAllColumnsNotInBoard()
+      .toPromise()
+      .then(columns => this.columns = columns)
+      .catch(err => console.log(err))
+      .finally();
+  }
 
 
 }
